f8edd494b673f7023cb44565d6d43a9d7dd12a91
ShiningRay <tsowly@hotmail.com>
2018-11-24 02:46:53 +0800

    添加了两步验证相关内容

--- lib/account_service/oauth/client.rb
+++ lib/account_service/oauth/client.rb
@@ -4,11 +4,12 @@ module BitRabbit::AccountService
   module OAuth
     class Client < ::OAuth2::AccessToken
       BaseURL = '/oauth/v1'
-      def transfer(sn:, currency:, to:, amount:)
+      def transfer(sn:, currency:, to:, amount:, two_factor: nil)
         body = {
           sn: sn,
           currency: currency,
-          amount: amount
+          amount: amount,
+          two_factor: two_factor
         }
         if to.is_a?(Numeric)
           body[:to_member_id] = to
@@ -45,6 +46,20 @@ module BitRabbit::AccountService
         else
           raise res['errors']
         end
+      end
+
+      def send_sms_verification
+        res = post("#{BaseURL}/two_factors/send_sms_code").parsed
+        if res['success']
+          res['data']
+        else
+          raise res['errors']
+        end
+      end
+
+      def verify_two_factor(two_factor_params)
+        res = post("#{BaseURL}/two_factors/verify", body: {two_factor: two_factor_params}).parsed
+        res['success']
       end
     end
   end
